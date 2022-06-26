c8f732d0e5ecef6868a706d9d0bbd79161838cc7
sanjay <89442968+fgfhjk@users.noreply.github.com>
2022-06-26 20:26:49 +0530

    Update a2.scala

--- a2.scala
+++ a2.scala
@@ -1,6 +1,26 @@
+Demo
+To integrate spark with Delta Lake add a library dependency to build.sb
+
+libraryDependencies += "io.delta" %% "delta-core" % "1.0.1"
+
+(for Spark 3.2.1)
+
+to know your version compatiblity click on this URL
+
+https://docs.delta.io/latest/releases.html
+
+Example:
+
+▪ Read CSV file (people.csv) into dataframe
+
+▪ Write the data in the above dataframe to local filesystem in delta format
+
+▪ Read the above data written in the delta format into dataframe
+
+▪ Show record count for above dataframe
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.execution.command.RunnableCommand
 object a2 {
 
   def main(args: Array[String]) {
@@ -15,11 +35,13 @@ object a2 {
       .option("header", "True")
       .option("inferschema", "True")
       .load("C:/Users/user/Desktop/Assessment/people.csv")
-
+// writing dataframe to Delta
     df.write.format("delta").mode("overwrite").save("C:/Users/user/Desktop/A")
+ // reading Delta from Local to Dataframe   
 val df2=spark.read.format("delta").load("C:/Users/user/Desktop/A")
-  df2.show(100)
-    println("total count of reows are"+ df2.count())
+  df2.show()
+  //total count of records  
+    println("total count of records"+ df2.count())
 
 
   }
